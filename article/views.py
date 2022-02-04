a9d0a19b6c5ae4df61b0c6fbd514fa0ef7672d0c
aliyevorkhan <orxanaliyev19@gmail.com>
2022-02-05 01:04:51 +0400

    django urls and views added

--- article/views.py
+++ article/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def index(request):
+    # return HttpResponse("ESAS SEHIFE")
+    return render(request, "article/index.html")
