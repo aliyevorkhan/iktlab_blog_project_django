60cca26064a161652fafa3720fc6cb5293acff8a
aliyevorkhan <orxanaliyev19@gmail.com>
2022-02-04 15:26:06 +0400

    admin panel customized

--- article/admin.py
+++ article/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Article
 
-admin.site.register(Article) 
+# admin.site.register(Article) 
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "created_date"]
+    list_display_links = ["title", "created_date"]
+    search_fields = ["title"]
+    list_filter = ("created_date", "author")
+    
+    class Meta:
+        model = Article
--- article/models.py
+++ article/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 class Article(models.Model):
-    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, verbose_name="Yazar")
+    title = models.CharField(max_length=50, verbose_name="Başlıq")
+    content = models.TextField(verbose_name="Məzmun")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Yaradılma tarixi")
 
+    def __str__(self):
+        return self.title
+
+     
